4b69ba9f649a6bbb716a59754ef145d5a8227638
Blank67 <hmnshshrm67@gmail.com>
2022-12-13 14:57:43 +0530

    home page added

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,7 +1,87 @@
+import { Fragment } from "react";
+import './Home.css';
+
 const Home = (props) => {
-    return(
-        <h1>HOME PAGE</h1>
+    return (
+        <Fragment>
+            <h1 className="text-center m-2">Tours</h1>
+            <div className="px-5">
+                <div className='show-box'>
+                    <div> Sep 10</div>
+                    <div>DETROIT, MI</div>
+                    <div>DTE ENERGY MUSIC THEATRE</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 11</div>
+                    <div>TORONTO,ON</div>
+                    <div>BUDWEISER STAGE</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 12</div>
+                    <div>BRISTOW, VA</div>
+                    <div>JIGGY LUBE LIVE</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 13</div>
+                    <div>PHOENIX, AZ</div>
+                    <div>AK-CHIN PAVILION</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 14</div>
+                    <div>LAS VEGAS, NV</div>
+                    <div>AK-CHIN PAVILION</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 15</div>
+                    <div>PHOENIX, AZ</div>
+                    <div>AK-CHIN PAVILION</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+            </div>
+        </Fragment>
     );
 }
 
 export default Home;
+
+{/*             <div className='show-box'>
+                    <div> Sep 10</div>
+                    <div>DETROIT, MI</div>
+                    <div>DTE ENERGY MUSIC THEATRE</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 11</div>
+                    <div>TORONTO,ON</div>
+                    <div>BUDWEISER STAGE</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 12</div>
+                    <div>BRISTOW, VA</div>
+                    <div>JIGGY LUBE LIVE</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 13</div>
+                    <div>PHOENIX, AZ</div>
+                    <div>AK-CHIN PAVILION</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 14</div>
+                    <div>LAS VEGAS, NV</div>
+                    <div>AK-CHIN PAVILION</div>
+                    <div><button>Buy Tickets</button></div>
+                </div>
+                <div className='show-box'>
+                    <div> Sep 15</div>
+                    <div>PHOENIX, AZ</div>
+                    <div>AK-CHIN PAVILION</div>
+                    <div><button>Buy Tickets</button></div>
+                </div> */}
